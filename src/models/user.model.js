87c54aca2e2f1e8fe3fa3926df5f23e9d1bc55ac
Prafull <87638003+rjcodedev@users.noreply.github.com>
2025-07-22 10:06:29 +0530

    update fullname to fullName parameter in userSchema

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -19,7 +19,7 @@ const userSchema = new mongoose.Schema(
       lowercase: true,
       trim: true,
     },
-    fullname: {
+    fullName: {
       type: String,
       required: true,
       trim: true,
@@ -67,7 +67,7 @@ userSchema.methods.generateAccessToken = async function () {
     _id: this._id,
     email: this.email,
     username: this.username,
-    fullname: this.fullname,
+    fullName: this.fullName,
   };
   return jwt.sign(payload, process.env.ACCESS_TOKEN_SECRET, {
     expiresIn: process.env.ACCESS_TOKEN_EXPIRY,
